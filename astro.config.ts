1826a0d57d96d28006ddbfd971b9d31ce99946ea
Kot <kot@yukata.dev>
2024-01-22 21:02:48 -0600

    Revert to old design

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://yukata.dev",
+	output: "server",
+	adapter: cloudflare(),
 });
